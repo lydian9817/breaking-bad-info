ece66770b40d7a5978232dc4154216dfea8e0e20
lydian9817 <mauricio.caricari@gmail.com>
2022-08-26 18:40:17 -0300

    añado getCharacters()

--- app/src/main/java/com/example/breakingbadinfo/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/breakingbadinfo/overview/OverviewViewModel.kt
@@ -1,8 +1,27 @@
 package com.example.breakingbadinfo.overview
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.breakingbadinfo.network.BreakingBadApi
+import kotlinx.coroutines.launch
 
 class OverviewViewModel : ViewModel() {
 
+    //_status holds request results
+    private val _status = MutableLiveData<String>()
+    //backing property
+    val status: LiveData<String> = _status
 
+    init {
+        getCharacters()
+    }
+
+    private fun getCharacters() {
+        viewModelScope.launch {
+            val listResult = BreakingBadApi.retrofitService.getCharacters()
+            _status.value = listResult
+        }
+    }
 }
