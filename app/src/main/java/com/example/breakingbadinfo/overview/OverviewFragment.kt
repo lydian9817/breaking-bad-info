fbdaa47e3b68fa68a29d08aad4df2356a427914d
lydian9817 <mauricio.caricari@gmail.com>
2022-08-26 19:24:01 -0300

    OverviewFragment setup

--- app/src/main/java/com/example/breakingbadinfo/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/breakingbadinfo/overview/OverviewFragment.kt
@@ -1,4 +1,30 @@
 package com.example.breakingbadinfo.overview
 
-class OverviewFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.breakingbadinfo.databinding.FragmentOverviewBinding
+import androidx.fragment.app.viewModels
+
+class OverviewFragment : Fragment(){
+    private val viewModel: OverviewViewModel by viewModels()
+
+    /** Inflates the layout using data binding*/
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentOverviewBinding.inflate(inflater)
+
+        //allows Data Binding to observe LiveData
+        binding.lifecycleOwner = this
+
+        //gives binding acces to the viewModel
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
 }
