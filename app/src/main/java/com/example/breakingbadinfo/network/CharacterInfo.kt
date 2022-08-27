f0743499eba63d1d7eb20094a688ab0a1ad8dc25
lydian9817 <mauricio.caricari@gmail.com>
2022-08-26 22:01:36 -0300

    Defined the Character data class

--- app/src/main/java/com/example/breakingbadinfo/network/CharacterInfo.kt
+++ app/src/main/java/com/example/breakingbadinfo/network/CharacterInfo.kt
@@ -1,4 +1,17 @@
 package com.example.breakingbadinfo.network
 
-class CharacterInfo {
-}
+import com.squareup.moshi.Json
+
+data class CharacterInfo(
+    @Json(name = "char_id") val id: Int,
+    val name: String,
+    val birthday: String,
+    val occupation: List<String>,
+    val img: String,
+    val status: String,
+    val nickname: String,
+    val appearance: List<Int>,
+    val portrayed: String,
+    val category: String,
+    @Json(name = "better_call_saul_appearance") val bcsAppearance: List<Int>
+)
