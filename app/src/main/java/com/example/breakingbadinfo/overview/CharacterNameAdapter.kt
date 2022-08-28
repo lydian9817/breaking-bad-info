94c3ffc429bcf710da84cc60894243b403ae37da
lydian9817 <mauricio.caricari@gmail.com>
2022-08-28 01:11:50 -0300

    Implemented CharacterNameAdapter

--- app/src/main/java/com/example/breakingbadinfo/overview/CharacterNameAdapter.kt
+++ app/src/main/java/com/example/breakingbadinfo/overview/CharacterNameAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.breakingbadinfo.overview
 
-class CharacterNameAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import com.example.breakingbadinfo.network.Character
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.breakingbadinfo.databinding.CharacterNameItemBinding
+
+
+class CharacterNameAdapter : ListAdapter<Character,
+        CharacterNameAdapter.CharacterNameViewHolder>(DiffCallback){
+
+    class CharacterNameViewHolder(private var binding: CharacterNameItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(character: Character) {
+                binding.apply {
+                    characterNameTxt.text = character.name
+                    executePendingBindings()
+                }
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterNameAdapter.CharacterNameViewHolder {
+        return CharacterNameViewHolder(CharacterNameItemBinding.inflate(
+            LayoutInflater.from(parent.context)
+        ))
+    }
+
+    override fun onBindViewHolder(holder: CharacterNameAdapter.CharacterNameViewHolder, position: Int) {
+        val characterName = getItem(position)
+        holder.bind(characterName)
+    }
+
+    //checks repeated characters
+    companion object DiffCallback : DiffUtil.ItemCallback<Character>() {
+        override fun areItemsTheSame(oldItem: Character, newItem: Character): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Character, newItem: Character): Boolean {
+            return oldItem.name == newItem.name
+        }
+    }
+
 }
