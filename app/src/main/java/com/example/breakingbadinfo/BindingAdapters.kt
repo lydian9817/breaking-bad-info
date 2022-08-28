6403963d0e0c65069613f183080660e60c115045
lydian9817 <mauricio.caricari@gmail.com>
2022-08-28 01:46:27 -0300

    implemented listData property for the RecyclerView

--- app/src/main/java/com/example/breakingbadinfo/BindingAdapters.kt
+++ app/src/main/java/com/example/breakingbadinfo/BindingAdapters.kt
@@ -1,2 +1,15 @@
 package com.example.breakingbadinfo
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.breakingbadinfo.network.Character
+import com.example.breakingbadinfo.overview.CharacterNameAdapter
+
+//bindingadapter for the RecyclerView
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView,
+                     data: List<Character>?) {
+    val adapter = recyclerView.adapter as CharacterNameAdapter
+    adapter.submitList(data)
+
+}
